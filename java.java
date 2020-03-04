43b9f5d2a2d6a156939dbbbbe07c6adc041f0728
BlackCat <dm3-1-10.19@uni.eus>
2020-03-04 14:03:16 +0100

    Create java.java

---
+++ java.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class piramide {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    int num = in.nextInt();
+    for(int i = 1; i <= num; i ++){
+        for(int a = 1;a <= num; a++){
+            System.out.print(a);
+        }
+        System.out.println("");    
+    }
+  }
+}
